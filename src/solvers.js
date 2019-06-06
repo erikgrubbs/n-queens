50976d311af5134f659502a15b55e74adf6bc8a5
erikgrubbs <erikgrubbs@gmail.com>
2019-06-06 11:44:42 -0700

    applies style correctly

--- src/solvers.js
+++ src/solvers.js
@@ -67,7 +67,7 @@ window.countNRooksSolutions = function(n) {
       } 
       board.togglePiece(currentRow, i);
     }
-  }
+  };
   
   finder(0);
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
@@ -79,8 +79,8 @@ window.findNQueensSolution = function(n) {
   var board = new Board({
     n: n
   });
-  var solution = []
-  var toggles = true
+  var solution = [];
+  var toggles = true;
   if (n === 0) {
     return [];
   } else if (n === 1) {
@@ -92,7 +92,6 @@ window.findNQueensSolution = function(n) {
   var finder = function(currentRow) {    
     if (currentRow === n) {
       solution = board.rows();
-      console.log('working solution :', solution)
       toggles = false;
       return;
     }    
@@ -102,14 +101,14 @@ window.findNQueensSolution = function(n) {
       }
       board.togglePiece(currentRow, i);      
       if (!board.hasAnyQueensConflicts()) {
-       finder(currentRow + 1);
+        finder(currentRow + 1);
       }
       if (!toggles) {
         break;
       }
       board.togglePiece(currentRow, i);
     }
-  }
+  };
   finder(0);
   console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
   return solution;
@@ -117,14 +116,13 @@ window.findNQueensSolution = function(n) {
 
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
 window.countNQueensSolutions = function(n) {
-    var solutionCount = 0;
+  var solutionCount = 0;
   var board = new Board({
     n: n
   });
   if (n === 0 || n === 1) {
     return 1;
   }
-  
   var finder = function(currentRow) {    
     if (currentRow === n) {
       solutionCount++;
@@ -137,7 +135,7 @@ window.countNQueensSolutions = function(n) {
       } 
       board.togglePiece(currentRow, i);
     }
-  }
+  };
   finder(0);
   console.log('Number of solutions for ' + n + ' queens:', solutionCount);
   return solutionCount;
